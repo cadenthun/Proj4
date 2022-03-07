212ea09eaa8f43f196f82714a07513daa9a8ed66
Caden Thun <cadenthun@Cadens-MacBook-Air.local>
2022-03-07 15:01:57 -0800

    second commit

--- Project4/RadixTree.h
+++ Project4/RadixTree.h
@@ -2,36 +2,47 @@
 #define RADIXTREE
 
 #include <iostream>
-#include <unordered_map>
+#include <string>
 
 template <typename ValueType>
 class RadixTree {
+    
 public:
  RadixTree();
  ~RadixTree();
  void insert(std::string key, const ValueType& value);
 ValueType* search(std::string key) const;
+    
 private: //get rid of this once I implement the Radix Tree
   //  std::unordered_map<std::string, ValueType> m_tempMap;
+    bool m_empty = true; //HAVE TO CHANGE THIS TO FALSE AS SOON AS A NODE GETS ADDED
     struct Node
     {
-        Node* edges[180];
-        ValueType value;
+        std::string nodeString;
+        Node* edges[128];
+        ValueType* nodeValuePtr;
         bool endOfWord;
-        Node()
+        Node (std::string nString = "")
         {
-            for (int i = 0; i < 180; i++)
+            for (int i = 0; i < 128; i++)
                 edges[i] = nullptr;
+            nodeString = nString;
+        }
+        Node (ValueType nValue, std::string nString = "") //inserting a leaf node containing a value
+        {
+            nodeValuePtr = &nValue;
+            nodeString = nString;
+            endOfWord = true;
         }
     };
-    Node root;
-    
+    Node* m_root[128] = {nullptr};
 };
 
 template <typename ValueType>
 RadixTree<ValueType>::RadixTree()
 {
-    
+    for (int i = 0; i < 128; i++)
+        m_root[i] = nullptr;
 }
 
 template <typename ValueType>
@@ -44,17 +55,56 @@ RadixTree<ValueType>::~RadixTree()
 template <typename ValueType>
 void RadixTree<ValueType>::insert(std::string key, const ValueType &value)
 {
-    m_tempMap.insert_or_assign(key, value);
+    Node* currNode = nullptr; // for navigating through tree
+    
+    ValueType valueCopy = value; // where nodeValuePtr will eventually point to
+    
+    if (m_root[key.at(0)] == nullptr) // no words in tree that begin with starting letter of insetionString
+    {
+        int insertionIndex = key.at(0);
+        key.erase(0, 1);
+        if (key.size() == 0) // key was 1 char long
+        {
+            m_root[insertionIndex] = new Node(valueCopy, "");
+            return; // key/value pair inserted
+        }
+        m_root[insertionIndex] = new Node(valueCopy, key); // handle situation where key was more than 1 char long
+    }
+    
+    else
+    {
+        currNode = m_root[key.at(0)];
+        
+        key.erase(0, 1);
+        
+        if (currNode->nodeString == "")
+        {
+            if (currNode->edges[key.at(0)] == nullptr)
+            {
+                int insertionIndex = key.at(0);
+                key.erase(0, 1);
+                if (key.size() == 0) // remaining key was 1 char long
+                {
+                    currNode->edges[insertionIndex] = new Node(valueCopy, "");
+                    return; // 1 return condition for recursive function
+                }
+                currNode->edges[insertionIndex] = new Node(valueCopy, key); // key was more than 1 char long
+            }
+            else // currNode->edges[key.at(0)] != nullptr
+        }
+        else
+        {
+            // check insertionString against nodeString, handling each edge case as necessary
+        }
+        
+        
+    }
 }
 
 template <typename ValueType> 
 ValueType* RadixTree<ValueType>::search(std::string key) const
 {
-    typename std::unordered_map<std::string, ValueType>::const_iterator it;
-    it = m_tempMap.find(key);
-    if (it == m_tempMap.end())
-        return nullptr;
-    return &(it->second);
+    return nullptr; //DUMMY
 }
 
 #endif /* RADIXTREE */
